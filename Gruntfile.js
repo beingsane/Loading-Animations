52a03c2c17ac076b2dc7d68e49ca677d3014a394
Martin Skroch <m.skroch@mail.de>
2014-11-02 17:06:15 +0100

    changed version number

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,10 +7,9 @@ module.exports = function(grunt) {
 		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
 			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n\n',
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+			' License: <%= pkg.license %> */\n\n',
 		
-
 		less: {
 			css: {
 				options: {
